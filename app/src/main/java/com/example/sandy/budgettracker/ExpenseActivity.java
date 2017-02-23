beb1c24862abdaa8b875e85ec34edf4677651211
sandychowdary.535 <sandychowdary.535@gmail.com>
2017-02-23 15:01:14 -0600

    updated code

--- app/src/main/java/com/example/sandy/budgettracker/ExpenseActivity.java
+++ app/src/main/java/com/example/sandy/budgettracker/ExpenseActivity.java
@@ -1,16 +1,16 @@
 package com.example.sandy.budgettracker;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.Button;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.GridView;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ExpenseActivity extends AppCompatActivity {
 
@@ -24,66 +24,45 @@ public class ExpenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expence);
-        items = new ArrayList<ExpenseItem>();
-        items.add(new ExpenseItem("CAR", 0, 0));
-        items.add(new ExpenseItem("RENT", 0, 0));
-        items.add(new ExpenseItem("FOOD", 0, 0));
-        items.add(new ExpenseItem("GROCERIES", 0, 0));
-        items.add(new ExpenseItem("DRINKS", 0, 0));
-        items.add(new ExpenseItem("BILL", 0, 0));
-        items.add(new ExpenseItem("DRINKS", 0, 0));
-        items.add(new ExpenseItem("CLOTHES", 0, 0));
-        items.add(new ExpenseItem("HOBBIES", 0, 0));
-        items.add(new ExpenseItem("TRANSPORTATION", 0, 0));
-        items.add(new ExpenseItem("PETS", 0, 0));
-        items.add(new ExpenseItem("SHOPPING", 0, 0));
-        items.add(new ExpenseItem("HEALTHCARE", 0, 0));
-        items.add(new ExpenseItem("UTILITIES", 0, 0));
-        items.add(new ExpenseItem("HOME", 0, 0));
-        items.add(new ExpenseItem("ACCOMMODATION", 0, 0));
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        getSupportActionBar().setHomeButtonEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        gridView = (GridView) findViewById(R.id.expense);
-
-        itemsAdapter = new ExpenseItemAdapter(this, items);
-        gridView.setAdapter(itemsAdapter);
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
-                if (selectedPosition != -1) {
-                    // TextView textView = (TextView) gridView.getChildAt(selectedPosition).findViewById(R.id.expense_name);
-                    gridView.getChildAt(selectedPosition).setAlpha(1f);
-                }
-                gridView.getChildAt(position).setAlpha(.5f);
-//                TextView textView = (TextView) gridView.getChildAt(position).findViewById(R.id.expense_name);
-//                int color = ContextCompat.getColor(itemsAdapter.getContext(), R.color.colorPrimary);
-//                textView.setBackgroundColor(color);
-                TextView textView = (TextView) gridView.getChildAt(position).findViewById(R.id.expense_name);
-                expenseItem = textView.getText().toString();
-                selectedPosition = position;
-            }
-        });
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager1);
+        List<String> tabs = new ArrayList<String>();
+        tabs.add("EXPENSES");
+        tabs.add("INCOME");
 
 
-        Button b = (Button) findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Enter expense details", Toast.LENGTH_SHORT).show();
-                TextView amountTextView = (TextView) findViewById(R.id.amount);
-                double amount = Double.parseDouble(amountTextView.getText().toString());
-                openExpenseDetailActivity(v, expenseItem, amount);
-            }
-        });
 
+        Fragment f1 = new MonthFragment();
+        Bundle b1 = new Bundle();
+        b1.putString("type", "Expense");
+        f1.setArguments(b1);
+
+        Fragment f2 = new MonthFragment();
+        Bundle b2 = new Bundle();
+        b2.putString("type", "Income");
+        f2.setArguments(b2);
+
+
+        ArrayList<Fragment> fragments = new ArrayList<Fragment>();
+        fragments.add(f1);
+        fragments.add(f2);
+
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), fragments, tabs);
+        viewPager.setAdapter(adapter);
+
+        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs1);
+        // Attach the view pager to the tab strip
+        tabsStrip.setViewPager(viewPager);
 
     }
 
-    private void openExpenseDetailActivity(View view, String expenseItem, double amount) {
-        Intent intent = new Intent(this, ExpenseDetailActivity.class);
-        intent.putExtra("amount", amount);
-        intent.putExtra("expenseItem", expenseItem);
-        startActivity(intent);
-    }
 }
---
+++ app/src/main/java/com/example/sandy/budgettracker/ExpenseFragment.java
@@ -0,0 +1,102 @@
+package com.example.sandy.budgettracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class ExpenseFragment extends Fragment {
+
+    private ArrayList<ExpenseItem> items;
+    private int selectedPosition = -1;
+    private GridView gridView;
+    ExpenseItemAdapter itemsAdapter;
+    String expenseItem = null;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (getArguments().get("type") == "Expense") {
+            items = new ArrayList<ExpenseItem>();
+            items.add(new ExpenseItem("CAR", 0, 0));
+            items.add(new ExpenseItem("RENT", 0, 0));
+            items.add(new ExpenseItem("FOOD", 0, 0));
+            items.add(new ExpenseItem("GROCERIES", 0, 0));
+            items.add(new ExpenseItem("DRINKS", 0, 0));
+            items.add(new ExpenseItem("BILL", 0, 0));
+            items.add(new ExpenseItem("DRINKS", 0, 0));
+            items.add(new ExpenseItem("CLOTHES", 0, 0));
+            items.add(new ExpenseItem("HOBBIES", 0, 0));
+            items.add(new ExpenseItem("TRANSPORTATION", 0, 0));
+            items.add(new ExpenseItem("PETS", 0, 0));
+            items.add(new ExpenseItem("SHOPPING", 0, 0));
+            items.add(new ExpenseItem("HEALTHCARE", 0, 0));
+            items.add(new ExpenseItem("UTILITIES", 0, 0));
+            items.add(new ExpenseItem("HOME", 0, 0));
+            items.add(new ExpenseItem("ACCOMMODATION", 0, 0));
+        } else {
+            items = new ArrayList<ExpenseItem>();
+            items.add(new ExpenseItem("EXTRA INCOME", 0, 0));
+            items.add(new ExpenseItem("GIFTS", 0, 0));
+            items.add(new ExpenseItem("SALARY", 0, 0));
+            items.add(new ExpenseItem("LOAN", 0, 0));
+            items.add(new ExpenseItem("BUSINESS", 0, 0));
+
+        }
+
+
+        gridView = (GridView) getActivity().findViewById(R.id.expense);
+
+        itemsAdapter = new ExpenseItemAdapter(getActivity(), items);
+        gridView.setAdapter(itemsAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
+                if (selectedPosition != -1) {
+                    // TextView textView = (TextView) gridView.getChildAt(selectedPosition).findViewById(R.id.expense_name);
+                    gridView.getChildAt(selectedPosition).setAlpha(1f);
+                }
+                gridView.getChildAt(position).setAlpha(.5f);
+//                TextView textView = (TextView) gridView.getChildAt(position).findViewById(R.id.expense_name);
+//                int color = ContextCompat.getColor(itemsAdapter.getContext(), R.color.colorPrimary);
+//                textView.setBackgroundColor(color);
+                TextView textView = (TextView) gridView.getChildAt(position).findViewById(R.id.expense_name);
+                expenseItem = textView.getText().toString();
+                selectedPosition = position;
+            }
+        });
+
+
+//        Button b = (Button) getActivity().findViewById(R.id.button);
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(v.getContext(), "Enter expense details", Toast.LENGTH_SHORT).show();
+//                TextView amountTextView = (TextView) getActivity().findViewById(R.id.amount);
+//                double amount = Double.parseDouble(amountTextView.getText().toString());
+//                openExpenseDetailActivity(v, expenseItem, amount);
+//            }
+//        });
+        return inflater.inflate(R.layout.fragment_expense, container, false);
+    }
+
+    private void openExpenseDetailActivity(View view, String expenseItem, double amount) {
+        Intent intent = new Intent(getActivity(), ExpenseDetailActivity.class);
+        intent.putExtra("amount", amount);
+        intent.putExtra("expenseItem", expenseItem);
+        startActivity(intent);
+    }
+
+
+}
