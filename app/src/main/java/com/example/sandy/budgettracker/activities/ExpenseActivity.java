13d2cf8d35dc4b1b865fd52c97102f7dee0332cd
Sundeep Chilukuri <sundeep.chilukur@smail.astate.edu>
2017-03-17 14:00:07 -0500

    Updated code

--- app/src/main/java/com/example/sandy/budgettracker/activities/ExpenseActivity.java
+++ app/src/main/java/com/example/sandy/budgettracker/activities/ExpenseActivity.java
@@ -2,6 +2,7 @@ package com.example.sandy.budgettracker.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +11,8 @@ import com.astuetz.PagerSlidingTabStrip;
 import com.example.sandy.budgettracker.R;
 import com.example.sandy.budgettracker.adapters.SimpleFragmentPagerAdapter;
 import com.example.sandy.budgettracker.fragments.ExpenseFragment;
+import com.example.sandy.budgettracker.fragments.ExpenseSelectionFragment;
+import com.example.sandy.budgettracker.fragments.TransactionsFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +24,7 @@ public class ExpenseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expence);
 
-
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.expenseToolbar);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -31,38 +32,10 @@ public class ExpenseActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.contentExpense, new ExpenseSelectionFragment());
+        transaction.commit();
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager1);
-        List<String> tabs = new ArrayList<>();
-        tabs.add("EXPENSES");
-        tabs.add("INCOME");
-
-
-        Fragment f1 = new ExpenseFragment();
-        Bundle b1 = new Bundle();
-        b1.putString("type", "Expense");
-        f1.setArguments(b1);
-
-        Fragment f2 = new ExpenseFragment();
-        Bundle b2 = new Bundle();
-        b2.putString("type", "Income");
-        f2.setArguments(b2);
-
-
-        ArrayList<Fragment> fragments = new ArrayList<>();
-        fragments.add(f1);
-        fragments.add(f2);
-
-
-        // Create an adapter that knows which fragment should be shown on each page
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), fragments, tabs);
-        if (viewPager != null)
-            viewPager.setAdapter(adapter);
-
-        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs1);
-        // Attach the view pager to the tab strip
-        if (tabsStrip != null && viewPager != null)
-            tabsStrip.setViewPager(viewPager);
 
     }
 
---
+++ app/src/main/java/com/example/sandy/budgettracker/fragments/ExpenseSelectionFragment.java
@@ -0,0 +1,60 @@
+package com.example.sandy.budgettracker.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.example.sandy.budgettracker.R;
+import com.example.sandy.budgettracker.adapters.SimpleFragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpenseSelectionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_expense_selection, container, false);
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager1);
+        List<String> tabs = new ArrayList<>();
+        tabs.add("EXPENSES");
+        tabs.add("INCOME");
+
+
+        Fragment f1 = new ExpenseFragment();
+        Bundle b1 = new Bundle();
+        b1.putString("type", "Expense");
+        f1.setArguments(b1);
+
+        Fragment f2 = new ExpenseFragment();
+        Bundle b2 = new Bundle();
+        b2.putString("type", "Income");
+        f2.setArguments(b2);
+
+
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(f1);
+        fragments.add(f2);
+
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getChildFragmentManager(), fragments, tabs);
+        if (viewPager != null)
+            viewPager.setAdapter(adapter);
+
+        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) view.findViewById(R.id.tabs1);
+        // Attach the view pager to the tab strip
+        if (tabsStrip != null && viewPager != null)
+            tabsStrip.setViewPager(viewPager);
+        return view;
+    }
+
+}
