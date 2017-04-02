6c3ad208971afe1fb0994aef138f7d270f3d7636
sandychowdary.535 <sandychowdary.535@gmail.com>
2017-04-02 12:54:56 -0500

    updated code

--- app/src/main/java/com/example/sandy/budgettracker/adapters/BudgetsAdapter.java
+++ app/src/main/java/com/example/sandy/budgettracker/adapters/BudgetsAdapter.java
@@ -2,6 +2,7 @@ package com.example.sandy.budgettracker.adapters;
 
 import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,27 +12,47 @@ import com.example.sandy.budgettracker.R;
 import com.example.sandy.budgettracker.data.BudgetData;
 import com.example.sandy.budgettracker.helper.CustomProgress;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class BudgetsAdapter extends RecyclerView.Adapter<BudgetsAdapter.CustomViewHolder> {
+public class BudgetsAdapter extends RecyclerView.Adapter<BudgetsAdapter.GeneralViewHolder> {
     private ArrayList<BudgetData> budgetDatas;
     private Activity activity;
 
-    class CustomViewHolder extends RecyclerView.ViewHolder {
-        TextView budgetNameTextView;
+    class GeneralViewHolder extends RecyclerView.ViewHolder {
+        GeneralViewHolder(View view) {
+            super(view);
+        }
+    }
 
+    class CustomViewHolder extends GeneralViewHolder {
+        TextView budgetNameTextView;
+        TextView totalBudgetExpenseTextView;
         CustomProgress customProgressShowProgress;
+        TextView budgetStartDateTextView;
+        TextView budgetEndDateTextView;
+
+        TextView addBudgetTextView;
 
         CustomViewHolder(View view) {
             super(view);
             budgetNameTextView = (TextView) view.findViewById(R.id.budgetName);
-
+            totalBudgetExpenseTextView = (TextView) view.findViewById(R.id.totalBudgetExpense);
             customProgressShowProgress = (CustomProgress) view.findViewById(R.id.customProgressShowProgress);
-            customProgressShowProgress.setMaximumPercentage(0.5f);
-           // customProgressShowProgress.useRoundedRectangleShape(30.0f);
-            customProgressShowProgress.setProgressColor(view.getResources().getColor(R.color.green_500));
-            customProgressShowProgress.setProgressBackgroundColor(view.getResources().getColor(R.color.green_200));
-            customProgressShowProgress.setShowingPercentage(true);
+            budgetStartDateTextView = (TextView) view.findViewById(R.id.budgetStartDate);
+            budgetEndDateTextView = (TextView) view.findViewById(R.id.budgetEndDate);
+
+            addBudgetTextView = (TextView) view.findViewById(R.id.add);
+
+        }
+
+    }
+
+    class AddBudgetViewHolder extends GeneralViewHolder {
+
+        AddBudgetViewHolder(View view) {
+            super(view);
 
         }
 
@@ -43,16 +64,49 @@ public class BudgetsAdapter extends RecyclerView.Adapter<BudgetsAdapter.CustomVi
     }
 
     @Override
-    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.budgets_list, null);
-        return new CustomViewHolder(view);
+    public int getItemViewType(int position) {
+        //Some logic to know which type will come next;
+        return position == budgetDatas.size() - 1 ? -1 : position;
     }
 
     @Override
-    public void onBindViewHolder(CustomViewHolder holder, int position) {
+    public GeneralViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        GeneralViewHolder holder;
+        View view;
+        if (viewType == -1) {
+            view = LayoutInflater.from(activity)
+                    .inflate(R.layout.add_budget_card, viewGroup, false);
 
-        holder.budgetNameTextView.setText("sandy " + position);
+            holder = new AddBudgetViewHolder(view); //Of type GeneralViewHolder
+        } else {
+            view = LayoutInflater.from(activity)
+                    .inflate(R.layout.budgets_list, viewGroup, false);
+            holder = new CustomViewHolder(view);
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(GeneralViewHolder holder, int position) {
+        Log.v("@@@@@@@@@@@@", position + "  ");
+        if (getItemViewType(position) == -1) {
+            AddBudgetViewHolder holder1 = (AddBudgetViewHolder) holder;
+        } else {
+            CustomViewHolder holder1 = (CustomViewHolder) holder;
+            holder1.budgetNameTextView.setText(budgetDatas.get(position).getBudgetName());
+            holder1.totalBudgetExpenseTextView.setText("$500 out of $" + budgetDatas.get(position).getBudgetAmount());
+            holder1.customProgressShowProgress.setMaximumPercentage(0.5f);
+            // customProgressShowProgress.useRoundedRectangleShape(30.0f);
+            holder1.customProgressShowProgress.setProgressColor(activity.getResources().getColor(R.color.green_500));
+            holder1.customProgressShowProgress.setProgressBackgroundColor(activity.getResources().getColor(R.color.green_200));
+            holder1.customProgressShowProgress.setShowingPercentage(true);
+            try {
+                holder1.budgetStartDateTextView.setText((new SimpleDateFormat("MMM, yyyy", Locale.US).parse(budgetDatas.get(position).getStartDate().toString())).toString());
+                holder1.budgetEndDateTextView.setText((new SimpleDateFormat("MMM, yyyy", Locale.US).parse(budgetDatas.get(position).getEndDate().toString())).toString());
+            } catch (Exception e) {
+
+            }
+        }
     }
 
     @Override
--- app/src/main/java/com/example/sandy/budgettracker/fragments/BudgetFragment.java
+++ app/src/main/java/com/example/sandy/budgettracker/fragments/BudgetFragment.java
@@ -1,5 +1,6 @@
 package com.example.sandy.budgettracker.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,37 +10,61 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sandy.budgettracker.R;
+import com.example.sandy.budgettracker.activities.AddBudgetActivity;
 import com.example.sandy.budgettracker.adapters.BudgetsAdapter;
-import com.example.sandy.budgettracker.adapters.TransactionsAdapter;
+import com.example.sandy.budgettracker.adapters.RecyclerItemClickListener;
 import com.example.sandy.budgettracker.data.BudgetData;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class BudgetFragment extends Fragment {
 
-    private View view;
-    private RecyclerView recyclerView;
-    private BudgetsAdapter itemsAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        ArrayList<BudgetData> budgetDatas = new ArrayList<BudgetData>();
+        View view;
+        final RecyclerView recyclerView;
+        BudgetsAdapter itemsAdapter;
+
+        final ArrayList<BudgetData> budgetDatas = new ArrayList<>();
         view = inflater.inflate(R.layout.fragment_budget, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.budgets);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        budgetDatas.add(new BudgetData("sandy 0", 1000, new Date(), new Date()));
+        budgetDatas.add(new BudgetData("sandy 1", 1000, new Date(), new Date()));
 
+        //TODO : Query the database for budgets list
         budgetDatas.add(new BudgetData());
-        budgetDatas.add(new BudgetData());
-
 
         itemsAdapter = new BudgetsAdapter(this.getActivity(), budgetDatas);
         recyclerView.setAdapter(itemsAdapter);
 
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(this.getContext(), new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+//                        View viewItem = recyclerView.getLayoutManager().findViewByPosition(position);
+                        if (position == budgetDatas.size() - 1) {
+                            openExpenseActivity();
+                        }
+
+                    }
+                })
+        );
+
         return view;
+    }
+
+    private void openExpenseActivity() {
+        Intent intent = new Intent(this.getActivity(), AddBudgetActivity.class);
+        startActivity(intent);
+        getActivity().overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
 
 }
